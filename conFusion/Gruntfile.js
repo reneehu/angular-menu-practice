7677b8ea3fb69603792671fa3e982766a8c6cd7a
reneehu <reneehu217@gmail.com>
2016-05-21 10:09:05 -0400

    ewwww revert some package file

--- conFusion/Gruntfile.js
+++ conFusion/Gruntfile.js
@@ -1,10 +1,37 @@
 "use strict";
 
 module.exports = function (grunt) {
-	
+    //Time how long taks take. cam help when optimizing build times
+    require('time-grunt')(grunt);
+
+	//Automatically load rquired Grunt tasks 
+	require('jit-grunt')(grunt);
+
 //define the configuration for all the tasks
 grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+
+// make sure code styles are up to par
+jshint: { 
+	options:{
+		jshintrc: '.jshintrc',
+		reporter: require('jshint-stylish')
+	},
+	all: {
+		src: [
+		'Gruntfile.js',
+		'app/scripts/{,*/}*.js'
+		]
+	}
+}
 
 });
 
+	grunt.registerTask('build',[
+		'jshint'
+	]);
+
+	grunt.registerTask('default',['build']);
+
 };
+
